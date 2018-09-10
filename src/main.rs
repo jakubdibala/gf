174dfcf428295a7f06a23b1605d493f5a52095d1
Jakub Dibala <jakub.dibala@brainz.cz>
2018-09-10 19:57:26 +0200

    Add clap extern crate for command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 extern crate colored;
 extern crate git2;
+extern crate clap;
 
 use std::path::Path;
 use colored::*;
+use clap::{Arg, App, SubCommand};
 
 mod repository;
+
+#[derive(Debug)]
+enum Verbosity {
+    Quiet,
+    Verbose
+}
 
 
 const REPO_PATH: &str = "/Users/jd/code/gitflow-example";
@@ -14,14 +22,6 @@ const REPO_PATH: &str = "/Users/jd/code/gitflow-example";
  * Main function 
  */
 fn main() {
-
-
-    // check git repo
-    if !check_git_is_initialized() {
-        log_error("Git repository is not initialized here!");
-        return;
-    }
-
     // open repository
     let repo = match git2::Repository::open(Path::new(REPO_PATH)) {
         Ok(r)       => r,
@@ -31,19 +31,32 @@ fn main() {
         }
     };
 
-    let current = repository::current_branch(repo);
+    // configure app args
+    let matches = App::new("Git flow tool")
+                    .version("1.0")
+                    .author("Jakub Dibala <jakub.dibala@brainz.cz>")
+                    .about("This tool is an easier way to use git flow (the right way)")
+                    .arg(
+                        Arg::with_name("verbose")
+                            .short("v")
+                            .long("verbose")
+                            .help("Enables verbose output")
+                    )
+                    .arg(
+                        Arg::with_name("target")
+                            .help("feature/hotfix/release name")
+                    )
+                    .get_matches();
+
+    // get verbose level
+    let verbosity = match matches.occurrences_of("verbose") {
+        0 => Verbosity::Quiet,
+        _ => Verbosity::Verbose,
+    };
+
+    println!("{:?}", verbosity);
 }
 
-
-/**
- * Check if git repository is
- * initialized in current directory
- */
-fn check_git_is_initialized() -> bool {
-    let mut p: String = String::from(REPO_PATH);
-    p.push_str("/.git");
-    Path::new(&p).exists()
-}
 
 
 /**
